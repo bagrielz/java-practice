d8237f0ad2f36e12cb4480e3c86aa35744ec8823
Gabriel Stênio <89270315+bagrielz@users.noreply.github.com>
2024-11-08 11:45:11 -0300

    feat: Novos comentários

--- src/Leitura.java
+++ src/Leitura.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Leitura {
     public static void main(String[] args) {
+        // A classe Scanner é utilizada para ler dados de entrada em um programa Java
+        // Primeiro, importamos a classe e, em seguida, criamos uma instância dela passando o objeto System.in como parâmetro
+        // para indicar que queremos ler a entrada do usuário pelo teclado
         Scanner leitura = new Scanner(System.in);
 
         System.out.println("Digite seu filme favorito");
-        String filme = leitura.nextLine();
+        String filme = leitura.nextLine(); // nextLine() lê uma linha de texto
         System.out.println("Digite o ano de lançamento");
-        int anoDeLancamento = leitura.nextInt();
+        int anoDeLancamento = leitura.nextInt(); // nextLine() lê uma linha de número inteiro
         System.out.println("Digite a sua avaliação de 1 à 10");
-        double avaliacao = leitura.nextDouble();
+        double avaliacao = leitura.nextDouble(); // nextLine() lê uma linha de número decimal
 
         System.out.println(filme);
         System.out.println(anoDeLancamento);
